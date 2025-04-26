2b8fa724011af2a4910d852bde2e9784ef634698
Sylveria Ozioma Ihemedu <silvernasa@myyahoo.comclear>
2025-04-26 22:56:01 +0200

    Updated storyhubs feedbacks app and likes app

--- storyhubs_feedbacks/views.py
+++ storyhubs_feedbacks/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.db.models import Count
+from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from storyhubs_api.permissions import IsOwnerOrReadOnly
+from .models import StoryFeedback
+from .serializers import StoryhubsFeedbackSerializer
 
-# Create your views here.
+
+class StoryhubsFeedbackList(generics.ListCreateAPIView):
+    serializer_class = StoryhubsFeedbackSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = StoryhubsFeedback.objects.annotate(
+        likes_count=Count('likes', distinct=True),
+    ).order_by('-created_at')
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'owner__followed__owner__profile',
+        'likes__owner__profile',
+        'owner__profile',
+    ]
+    search_fields = [
+        'owner__username',
+        'story_title',
+        'story_creator',
+        'category',
+    ]
+    ordering_fields = [
+        'likes_count',
+        'likes__created_at',
+    ]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class StoryhubsFeedbackDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = StoryhubsFeedbackSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = StoryFeedback.objects.annotate(
+        likes_count=Count('likes', distinct=True),
+    ).order_by('-created_at')
