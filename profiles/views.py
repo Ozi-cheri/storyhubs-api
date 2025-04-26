8f51fb334076e53aeb0ddd9f516fe3bae6cb134b
Sylveria Ozioma Ihemedu <silvernasa@myyahoo.comclear>
2025-04-26 17:56:28 +0200

    Modify profiles serializers, views and update storyhubs feedbacks models.py

--- profiles/views.py
+++ profiles/views.py
@@ -1,40 +1,40 @@
-from django.db.models import Count
-from rest_framework import generics
-#from django_filters.rest_framework import DjangoFilterBackend
-#from storyhubs_api.permissions import IsOwnerOrReadOnly
+from django.http import Http404
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .models import Profile
 from .serializers import ProfileSerializer
 
 
-class ProfileList(generics.ListAPIView):
-    queryset = Profile.objects.annotate(
-        story_feedbacks_count=Count('owner__storyfeedback', distinct=True),
-        followers_count=Count('owner__followed', distinct=True),
-        following_count=Count('owner__following', distinct=True)
-    ).order_by('-created_at')
-    serializer_class = ProfileSerializer
-    #filter_backends = [
-        #filters.OrderingFilter,
-        #DjangoFilterBackend,
-    #]
-    #filterset_fields = [
-        'owner__following__followed__profile',
-        'owner__followed__owner__profile',
-    #]
-   # ordering_fields = [
-        'story_feedbacks_count',
-        'followers_count',
-        'following_count',
-        'owner__following__created_at',
-        'owner__followed__created_at',
-   # ]
+class ProfileList(APIView):
+    """
+    List all profiles
+    No Create view (post method), as profile creation handled by django signals
+    """
+    def get(self, request):
+        profiles = Profile.objects.all()
+        serializer = ProfileSerializer(profiles, many=True)
+        return Response(serializer.data)
 
 
-class ProfileDetail(generics.RetrieveUpdateAPIView):
-    #permission_classes = [IsOwnerOrReadOnly]
-    queryset = Profile.objects.annotate(
-        story_feedbacks_count=Count('owner__storyfeedback', distinct=True),
-        followers_count=Count('owner__followed', distinct=True),
-        following_count=Count('owner__following', distinct=True)
-    ).order_by('-created_at')
+class ProfileDetail(APIView):
     serializer_class = ProfileSerializer
+    def get_object(self, pk):
+        try:
+            profile = Profile.objects.get(pk=pk)
+            return profile
+        except Profile.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
